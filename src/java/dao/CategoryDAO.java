47ac9f7278854459589e4b1f656e20e43f2bfcc4
nhtoan611 <nhtoan611@gmail.com>
2017-05-24 10:21:32 +0700

    Display Product In Category

--- src/java/dao/CategoryDAO.java
+++ src/java/dao/CategoryDAO.java
@@ -24,17 +24,17 @@ public class CategoryDAO {
         ArrayList<Category> list = new ArrayList<Category>();
         while (rs.next()) {
             Category category = new Category();
-            category.setCategoryID(rs.getInt("category_id"));
+            category.setCategoryID(rs.getLong("category_id"));
             category.setCategoryName(rs.getString("category_name"));
             list.add(category);
         }
         return list;
     }
     
-    public static void main(String[] args) throws SQLException {
-        CategoryDAO dao=new CategoryDAO();
-        for(Category ds: dao.getListCategory()){
-            System.out.println(ds.getCategoryID()+"    "+ds.getCategoryName());
-        }
-    }
+//    public static void main(String[] args) throws SQLException {
+//        CategoryDAO dao=new CategoryDAO();
+//        for(Category ds: dao.getListCategory()){
+//            System.out.println(ds.getCategoryID()+"    "+ds.getCategoryName());
+//        }
+//    }
 }
---
+++ src/java/model/Product.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author nhtoan
+ */
+public class Product {
+    private long productID;
+    private long categoryID;
+    private String productName;
+    private String productImage;
+    private double productPrice;
+    private String productDesription;
+
+    public Product() {
+    }
+
+    public Product(long productID, long categoryID, String productName, String productImage, double productPrice, String productDesription) {
+        this.productID = productID;
+        this.categoryID = categoryID;
+        this.productName = productName;
+        this.productImage = productImage;
+        this.productPrice = productPrice;
+        this.productDesription = productDesription;
+    }
+
+    public long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(long productID) {
+        this.productID = productID;
+    }
+
+    public long getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(long categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDesription() {
+        return productDesription;
+    }
+
+    public void setProductDesription(String productDesription) {
+        this.productDesription = productDesription;
+    }
+    
+}
